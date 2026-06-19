438903c69d7a17084eaf8bbf0854562a208bdb09
brbrsxlegacy <torullu61@gmail.com>
2026-06-19 10:36:32 +0300

    Update Renderer.js

--- src/engine/Renderer.js
+++ src/engine/Renderer.js
@@ -12,7 +12,7 @@ export class Renderer {
     this.canvas.height = window.innerHeight;
   }
 
-  render(player) {
+  render(player, enemy, hitboxes) {
     const ctx = this.ctx;
 
     ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -28,7 +28,19 @@ export class Renderer {
     ctx.fillStyle = "#5E9C36";
     ctx.fillRect(0, this.canvas.height - 120, this.canvas.width, 120);
 
-    ctx.fillStyle = "#31D6FF";
+    ctx.fillStyle = player.color;
     ctx.fillRect(player.x, player.y, player.width, player.height);
+
+    ctx.fillStyle = enemy.color;
+    ctx.fillRect(enemy.x, enemy.y, enemy.width, enemy.height);
+
+    ctx.fillStyle = "rgba(255,255,255,0.45)";
+    for (const hitbox of hitboxes) {
+      ctx.fillRect(hitbox.x, hitbox.y, hitbox.width, hitbox.height);
+    }
+
+    ctx.fillStyle = "#000";
+    ctx.font = "24px Arial";
+    ctx.fillText(`${Math.floor(enemy.damage)}%`, 30, 40);
   }
 }
