2c4b1877c7b0b8f0d1a47d82edb42a6db010e1a6
brbrsxlegacy <torullu61@gmail.com>
2026-06-19 10:36:17 +0300

    Update Game.js

--- src/engine/Game.js
+++ src/engine/Game.js
@@ -8,8 +8,11 @@ export class Game {
     this.renderer = new Renderer();
     this.input = new Input();
     this.physics = new Physics();
-    this.player = new Player();
 
+    this.player = new Player(300, 100, "#31D6FF");
+    this.enemy = new Player(650, 100, "#FF5C8A");
+
+    this.hitboxes = [];
     this.running = false;
   }
 
@@ -34,10 +37,36 @@ export class Game {
       this.player.jump();
     }
 
+    if (this.input.pressed("j")) {
+      const hitbox = this.player.attack();
+      if (hitbox) this.hitboxes.push(hitbox);
+    }
+
     this.physics.update(this.player, this.renderer.canvas.height);
+    this.physics.update(this.enemy, this.renderer.canvas.height);
+
+    this.player.updateCooldowns();
+
+    for (const hitbox of this.hitboxes) {
+      hitbox.update();
+
+      if (hitbox.active && hitbox.intersects(this.enemy)) {
+        this.enemy.damage += hitbox.damage;
+
+        const dir = this.player.direction;
+        const force = hitbox.knockback * (1 + this.enemy.damage / 100);
+
+        this.enemy.vx += dir * force;
+        this.enemy.vy -= force * 0.45;
+
+        hitbox.active = false;
+      }
+    }
+
+    this.hitboxes = this.hitboxes.filter(h => h.active);
   }
 
   render() {
-    this.renderer.render(this.player);
+    this.renderer.render(this.player, this.enemy, this.hitboxes);
   }
 }
