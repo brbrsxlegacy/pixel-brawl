17d47c614291e3104796a5f74901ffc5a2c197f9
brbrsxlegacy <torullu61@gmail.com>
2026-06-19 11:57:19 +0300

    Update Input.js

--- src/engine/Input.js
+++ src/engine/Input.js
@@ -1,17 +1,31 @@
 export class Input {
   constructor() {
     this.keys = {};
+    this.locked = {};
 
     window.addEventListener("keydown", (e) => {
       this.keys[e.key.toLowerCase()] = true;
     });
 
     window.addEventListener("keyup", (e) => {
-      this.keys[e.key.toLowerCase()] = false;
+      const key = e.key.toLowerCase();
+      this.keys[key] = false;
+      this.locked[key] = false;
     });
   }
 
   pressed(key) {
     return !!this.keys[key.toLowerCase()];
+  }
+
+  justPressed(key) {
+    key = key.toLowerCase();
+
+    if (this.keys[key] && !this.locked[key]) {
+      this.locked[key] = true;
+      return true;
+    }
+
+    return false;
   }
 }
