801fa12d352f0fdafcaca05344129ae907099e07
brbrsxlegacy <torullu61@gmail.com>
2026-06-19 10:33:49 +0300

    Update Renderer.js

--- src/engine/Renderer.js
+++ src/engine/Renderer.js
@@ -4,10 +4,7 @@ export class Renderer {
     this.ctx = this.canvas.getContext("2d");
 
     this.resize();
-
-    window.addEventListener("resize", () => {
-      this.resize();
-    });
+    window.addEventListener("resize", () => this.resize());
   }
 
   resize() {
@@ -15,28 +12,23 @@ export class Renderer {
     this.canvas.height = window.innerHeight;
   }
 
-  render() {
+  render(player) {
     const ctx = this.ctx;
 
     ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    // gökyüzü
     ctx.fillStyle = "#87CEEB";
     ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
-    // güneş
     ctx.beginPath();
     ctx.arc(150, 120, 50, 0, Math.PI * 2);
     ctx.fillStyle = "#FFD54A";
     ctx.fill();
 
-    // zemin
     ctx.fillStyle = "#5E9C36";
-    ctx.fillRect(
-      0,
-      this.canvas.height - 120,
-      this.canvas.width,
-      120
-    );
+    ctx.fillRect(0, this.canvas.height - 120, this.canvas.width, 120);
+
+    ctx.fillStyle = "#31D6FF";
+    ctx.fillRect(player.x, player.y, player.width, player.height);
   }
 }
